a217249d1df57653e5f5c116ec2981d0611a8bcc
julia <j.kholstinina@yandex.ru>
2021-09-08 22:27:20 +0300

    add BottomSheet and get title with description of POD from server

--- app/src/main/java/com/example/nasaapp/view/picture/PODFragment.kt
+++ app/src/main/java/com/example/nasaapp/view/picture/PODFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -16,9 +17,11 @@ import com.example.nasaapp.databinding.FragmentPodBinding
 import com.example.nasaapp.model.PODData
 import com.example.nasaapp.utils.showSnackBar
 import com.example.nasaapp.viewmodel.PODViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class PODFragment : Fragment() {
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private var _binding: FragmentPodBinding? = null
     val binding: FragmentPodBinding
     get() {
@@ -43,10 +46,11 @@ class PODFragment : Fragment() {
         viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it) })
         viewModel.getPODFromServer()
         openWikiSearch()
+        setBottomSheetBehavior(binding.includeBottomSheetLayout.bottomSheetContainer)
     }
 
     private fun openWikiSearch() = with(binding) {
-        inputLayout.setEndIconOnClickListener {
+        inputTextLayout.setEndIconOnClickListener {
             val i = Intent(Intent.ACTION_VIEW).apply {
                 data = Uri.parse("https://en.wikipedia.org/wiki/${inputEditText.text.toString()}")
             }
@@ -57,25 +61,36 @@ class PODFragment : Fragment() {
     private fun renderData(data: PODData?)  {
         when(data) {
             is PODData.Error -> {
-                binding.includedLoadingLayout.loadingLayout.visibility = View.GONE
+                binding.includeLoadingLayout.loadingLayout.visibility = View.GONE
                 binding.main.showSnackBar(getString(R.string.error_appstate),
                     getString(R.string.reload_appstate), {viewModel.getPODFromServer()})
             }
             is PODData.Loading -> {
-                binding.includedLoadingLayout.loadingLayout.visibility = View.VISIBLE
+                binding.includeLoadingLayout.loadingLayout.visibility = View.VISIBLE
             }
             is PODData.Success -> {
-                binding.includedLoadingLayout.loadingLayout.visibility = View.GONE
-                val url = data.serverResponseData.hdurl
-                if (url.isNullOrEmpty()) {
-                    Toast.makeText(context, getString(R.string.error_url_empty), Toast.LENGTH_LONG).show()
-                } else {
-                    binding.customImageView.load(url) {
-                        error(R.drawable.ic_load_error_vector)
-                    }
-                }
+                binding.includeLoadingLayout.loadingLayout.visibility = View.GONE
+                setData(data)
             }
         }
+    }
+
+    private fun setData(data: PODData.Success) = with(binding) {
+        val url = data.serverResponseData.hdurl
+        if (url.isNullOrEmpty()) {
+            Toast.makeText(context, getString(R.string.error_url_empty), Toast.LENGTH_LONG).show()
+        } else {
+            customImageView.load(url) {
+                error(R.drawable.ic_load_error_vector)
+            }
+        }
+        includeBottomSheetLayout.bottomSheetDescriptionHeader.text = data.serverResponseData.title.toString()
+        includeBottomSheetLayout.bottomSheetDescription.text = data.serverResponseData.explanation.toString()
+    }
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
     override fun onDestroy() {
