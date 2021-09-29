c9e653a41c1e75d68292510601c3ac847bf116a0
julia <j.kholstinina@yandex.ru>
2021-09-29 22:23:20 +0300

    add quote span into about fragment

--- app/src/main/java/com/example/nasaapp/view/about/AboutAppFragment.kt
+++ app/src/main/java/com/example/nasaapp/view/about/AboutAppFragment.kt
@@ -1,6 +1,10 @@
 package com.example.nasaapp.view.about
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.QuoteSpan
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +12,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.transition.Slide
 import androidx.transition.TransitionManager
+import com.example.nasaapp.R
 import com.example.nasaapp.databinding.FragmentAboutBinding
 
 
@@ -47,5 +52,9 @@ class AboutAppFragment : Fragment() {
             logoIsVisible = !logoIsVisible
             binding.nasaLogoImageView.visibility = if (logoIsVisible) View.VISIBLE else View.GONE
         }
+
+        val spannable = SpannableStringBuilder(getString(R.string.about_quote))
+        spannable.setSpan(QuoteSpan(Color.BLUE), 0, spannable.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.aboutQuoteTextView.text = spannable
     }
 }
